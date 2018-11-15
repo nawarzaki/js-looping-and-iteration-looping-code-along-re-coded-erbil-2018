46e8f87bd3eb004d2079ab9807462018d0a57edd
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-15 15:24:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const employees = ['Lisa', 'Kaitlin', 'Jan']
 function printBadges( employees ) {
   for (let i=0; i<employees.length; i++){
-    console.log(`Welcome ${employees[i]} You are employee #1.`)
+    console.log(`Welcome ${employees[i]} You are employee .`)
   }
 }
 
 const employees2 = ['Joe', 'Gabe']
 function printBadges( employees2 ) {
   for (let i=0; i<employees.length; i++){
-    (`Welcome ${employees2[i]} You are employee #1.`)
+    (`Welcome ${employees2[i]} You are employee .`)
   }
 }
