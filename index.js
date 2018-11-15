82288e7e849f2f1016c019140ff6421fbff37bd0
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-15 15:19:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-const employees = ['Ada', 'Brendan', 'Ali']
+const employees = ['Lisa', 'Kaitlin', 'Jan']
 function printBadges( employees ) {
   for (let i=0; i<employees.length; i++){
     console.log(`Welcome ${employees[i]} You are employee #1.`)
