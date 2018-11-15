05ddee53872172ec3aa6c9a27c361a30803f1229
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-15 15:22:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function printBadges( employees ) {
 }
 
 const employees2 = ['Joe', 'Gabe']
+function printBadges( employees2 ) {
+  for (let i=0; i<employees.length; i++){
+    console.log(`Welcome ${employees2[i]} You are employee #1.`)
+  }
+}
+
