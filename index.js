8b789481e5e5eff83090fdc246833b5065adb7b7
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-29 13:01:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+let employees = ['Lisa', 'Kaitlin', 'Jan']
+function printBadges( employees ) {
+  for (let i=0; i<employees.length; i++){
+    console.log(`Welcome ${employees[i]} You are employee .`)
+  }
+  return employees
+}
+let employees2 = ['Joe', 'Gabe']
+function printBadges( employees2 ) {
+  for (let i=0; i<employees.length; i++){
+    (`Welcome ${employees2[i]} You are employee .`)
+  }
+  return employees2
+}
