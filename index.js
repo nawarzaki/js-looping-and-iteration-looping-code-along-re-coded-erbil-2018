3ee7077dbe52d34ba2c19417b84158bf2bf7f1ac
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-12-05 10:00:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+let emp = ['Lisa', 'Kaitlin', 'Jan']
+function printBadges(emp) {
+  for (let i=0; i<emp.length; i++){
+    console.log (`Welcome ${emp[i]}! You are employee #${i+1}.`)
+  }
+  return emp
+}
+
+function tailsNeverFails() {
+ let number = Math.random() 
+let result = ''
+while (if (number >=0.5){
+  result = Tails
+}else if(number<0.5){
+  result = heads
+}){
+  console.log(You got result tails in a row!)
+} 
+    
+
+return result
+}
